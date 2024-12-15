23355131e32599d99d3e823eb378cf1910fe7a60
evpesi <isaacson.evan@gmail.com>
2024-12-15 01:17:59 -0800

    Add notes to Snowflake Load SQL code

--- Snowflake_Load_from_S3_Bucket.sql
+++ Snowflake_Load_from_S3_Bucket.sql
@@ -2,6 +2,7 @@ CREATE WAREHOUSE CMS_WH;
 CREATE DATABASE CMS_DB;
 CREATE SCHEMA CMS_DB.prod;
 
+--Create a storage integration to connect to the S3 bucket
 CREATE STORAGE INTEGRATION my_s3_integration
 TYPE = EXTERNAL_STAGE
 STORAGE_PROVIDER = 'S3'
@@ -9,13 +10,16 @@ ENABLED = TRUE
 STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::396913706362:role/Snowflake-role-evan-data-project'
 STORAGE_ALLOWED_LOCATIONS = ('s3://evan-data-project/CMS_data/');
 
+--Gets information required to finish connecting AWS to S3 bucket
 DESC INTEGRATION my_s3_integration;
 
+--Create an external stage to load the data into
 CREATE OR REPLACE STAGE my_s3_stage
 URL = 's3://evan-data-project/CMS_data/'
 STORAGE_INTEGRATION = my_s3_integration
 FILE_FORMAT = csv_format;
 
+--Create a file format to load the data into 
 CREATE OR REPLACE FILE FORMAT csv_format
 TYPE = 'CSV'
 FIELD_DELIMITER = ','
@@ -25,6 +29,7 @@ EMPTY_FIELD_AS_NULL = true
 FIELD_OPTIONALLY_ENCLOSED_BY = '"'
 ESCAPE_UNENCLOSED_FIELD = None
 
+--Create a table to load the data into (the data is very dirty so we need to bring it in as strings or the load will fail)
 CREATE OR REPLACE TABLE CMS_Unplanned_Hospital_Visits
 (
     Facility_ID VARCHAR(10),
@@ -49,5 +54,6 @@ CREATE OR REPLACE TABLE CMS_Unplanned_Hospital_Visits
 	End_Date VARCHAR(50)
 );
 
+--Finally, load the data into the table!
 copy into CMS_Unplanned_Hospital_Visits from @my_s3_stage
 ON_ERROR = 'skip_file';
